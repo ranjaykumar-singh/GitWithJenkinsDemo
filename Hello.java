1e5aa042eb619b8c097e3467647693b9e7e68ec4
root <root@puppetm.eidiko.com>
2018-03-13 00:46:13 +0530

    change println message

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 public static void main(String args[]){
-System.out.println("Hello World");
-System.out.println("Hello India");
+System.out.println("Line 1");
+System.out.println("Line 2");
 }
 }
